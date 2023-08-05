d8708110b8871735deb1ec532438c29eafcf54bd
ssah3292 <100945367+ssah3292@users.noreply.github.com>
2023-08-05 18:13:31 +0530

    package practice

---
+++ src/geometry.py
@@ -0,0 +1,14 @@
+import shapes
+import numpy as np
+
+
+c = shapes.Circle(5)
+r = shapes.Rectangle(23.12, 45.82)
+s = shapes.Square(7.80)
+
+x = np.array([4, 6, 2, 8])
+print(np.median(x))
+
+print(c)
+print(r)
+print(s)
---
+++ src/mathtest.py
@@ -0,0 +1,5 @@
+from mathOpertaion import SayHello, power
+
+SayHello("sahoo")
+x = power(3, 2)
+print("power(3,2) : ", x)
